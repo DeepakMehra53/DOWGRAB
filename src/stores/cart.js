03606f0fbb1e95ffe16588eda2074e251438a944
DeepakMehra53 <dmehra594@gmail.com>
2025-03-27 20:51:05 +0530

    AddtoCart

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -17,8 +17,18 @@ const cartSlice = createSlice({
             else{
                 state.items.push({productId,quantity})
             }
-        }
+        },
+        changeQuantity(state, action) {
+            const { productId, quantity } = action.payload;
+            const indexProductId = (state.items).findIndex(item => item.productId === productId)
+            if (quantity > 0) {
+                state.items[indexProductId].quantity = quantity;
+            } else {
+                state.items = (state.items).filter(item => item.productId !== productId)
+            }
+            localStorage.setItem("carts", JSON.stringify(state.items));
+        },
     }
 })
-export const {addToCart} =cartSlice.actions
+export const { addToCart, changeQuantity } =cartSlice.actions
 export default cartSlice.reducer;
